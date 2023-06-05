b5f3d4d8196837c339f0b38777d485c3d2b6dbf9
Sweetleo30 <diana790@inbox.ru>
2023-06-05 16:42:39 +0300

    add adaptive cards lg

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './index.html';
 import './index.scss';
 // import code from './img/code.png'
-import { mult, sum } from './modules/calc';
+import cover from './img/cover.jpg'
+import img1 from './img/1.webp'
+import img2 from './img/2.webp'
+import img3 from './img/3.webp'
+import img4 from './img/4.webp'
+import img5 from './img/5.webp'
+import img6 from './img/6.webp'
+import img7 from './img/7.webp'
+import img8 from './img/8.webp'
+import img9 from './img/9.webp'
+import img10 from './img/10.webp'
+import img11 from './img/11.webp'
 
-// const imgWrap = document.querySelector('.img');
-// const img = new Image();
-// img.src = code;
-// img.width = 700;
-// imgWrap.append(img);
-
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+const animalsImg = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
