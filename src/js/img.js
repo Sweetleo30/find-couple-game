c98561d6edf345928ef0d352316a689d3f24d6ca
Sweetleo30 <diana790@inbox.ru>
2023-06-08 12:31:57 +0300

    added img.js

---
+++ src/js/img.js
@@ -0,0 +1,15 @@
+import imgCover from '../img/cover.jpg'
+import img1 from '../img/1.webp'
+import img2 from '../img/2.webp'
+import img3 from '../img/3.webp'
+import img4 from '../img/4.webp'
+import img5 from '../img/5.webp'
+import img6 from '../img/6.webp'
+import img7 from '../img/7.webp'
+import img8 from '../img/8.webp'
+import img9 from '../img/9.webp'
+import img10 from '../img/10.webp'
+import img11 from '../img/11.webp'
+
+export const animalsImg = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11];
+export const cover = imgCover; 
